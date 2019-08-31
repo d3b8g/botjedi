42833e0be0e7b3b79aa5efd969a0f15045e3184f
yoi3-0 <54473212+yoi3-0@users.noreply.github.com>
2019-08-31 22:55:01 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,24 +1,24 @@
 <? 
-define('CALLBACK_API_CONFIRMATION_TOKEN', '0c4514e0'); // Ñòðîêà, êîòîðóþ äîëæåí âåðíóòü ñåðâåð 
-define('VK_API_ACCESS_TOKEN', 'f93b8546c7c8801d5c9707ab82219b034d7a76cfd54090901c3d61c43e0510c90c764ce5bf38dfe4ac7ac'); // Êëþ÷ äîñòóïà ñîîáùåñòâà 
+define('CALLBACK_API_CONFIRMATION_TOKEN', '0c4514e0'); // Ã‘Ã²Ã°Ã®ÃªÃ , ÃªÃ®Ã²Ã®Ã°Ã³Ã¾ Ã¤Ã®Ã«Ã¦Ã¥Ã­ Ã¢Ã¥Ã°Ã­Ã³Ã²Ã¼ Ã±Ã¥Ã°Ã¢Ã¥Ã° 
+define('VK_API_ACCESS_TOKEN', 'f93b8546c7c8801d5c9707ab82219b034d7a76cfd54090901c3d61c43e0510c90c764ce5bf38dfe4ac7ac'); // ÃŠÃ«Ã¾Ã· Ã¤Ã®Ã±Ã²Ã³Ã¯Ã  Ã±Ã®Ã®Ã¡Ã¹Ã¥Ã±Ã²Ã¢Ã  
 
-define('CALLBACK_API_EVENT_CONFIRMATION', 'confirmation'); // Òèï ñîáûòèÿ î ïîäòâåðæäåíèè ñåðâåðà 
-define('CALLBACK_API_EVENT_MESSAGE_NEW', 'message_new'); // Òèï ñîáûòèÿ î íîâîì ñîîáùåíèè 
-define('VK_API_ENDPOINT', 'https://api.vk.com/method/'); // Àäðåñ îáðàùåíèÿ ê API 
-define('VK_API_VERSION', '5.89'); // Èñïîëüçóåìàÿ âåðñèÿ API 
+define('CALLBACK_API_EVENT_CONFIRMATION', 'confirmation'); // Ã’Ã¨Ã¯ Ã±Ã®Ã¡Ã»Ã²Ã¨Ã¿ Ã® Ã¯Ã®Ã¤Ã²Ã¢Ã¥Ã°Ã¦Ã¤Ã¥Ã­Ã¨Ã¨ Ã±Ã¥Ã°Ã¢Ã¥Ã°Ã  
+define('CALLBACK_API_EVENT_MESSAGE_NEW', 'message_new'); // Ã’Ã¨Ã¯ Ã±Ã®Ã¡Ã»Ã²Ã¨Ã¿ Ã® Ã­Ã®Ã¢Ã®Ã¬ Ã±Ã®Ã®Ã¡Ã¹Ã¥Ã­Ã¨Ã¨ 
+define('VK_API_ENDPOINT', 'https://api.vk.com/method/'); // Ã€Ã¤Ã°Ã¥Ã± Ã®Ã¡Ã°Ã Ã¹Ã¥Ã­Ã¨Ã¿ Ãª API 
+define('VK_API_VERSION', '5.89'); // ÃˆÃ±Ã¯Ã®Ã«Ã¼Ã§Ã³Ã¥Ã¬Ã Ã¿ Ã¢Ã¥Ã°Ã±Ã¨Ã¿ API 
 
 $event = json_decode(file_get_contents('php://input'), true); 
 
 switch ($event['type']) { 
-  // Ïîäòâåðæäåíèå ñåðâåðà 
+  // ÃÃ®Ã¤Ã²Ã¢Ã¥Ã°Ã¦Ã¤Ã¥Ã­Ã¨Ã¥ Ã±Ã¥Ã°Ã¢Ã¥Ã°Ã  
   case CALLBACK_API_EVENT_CONFIRMATION: 
     echo(CALLBACK_API_CONFIRMATION_TOKEN); 
     break; 
-  // Ïîëó÷åíèå íîâîãî ñîîáùåíèÿ 
+  // ÃÃ®Ã«Ã³Ã·Ã¥Ã­Ã¨Ã¥ Ã­Ã®Ã¢Ã®Ã£Ã® Ã±Ã®Ã®Ã¡Ã¹Ã¥Ã­Ã¨Ã¿ 
   case CALLBACK_API_EVENT_MESSAGE_NEW: 
     $message = $event['object']; 
     $peer_id = $message['peer_id'] ?: $message['user_id']; 
-    send_message($peer_id, "Ïîæàëóéñòà, ïîäïèøèñü íà ñîîáùåñòâî! (peer_id: {$peer_id})"); 
+    send_message($peer_id, "ÃÃ®Ã¦Ã Ã«Ã³Ã©Ã±Ã²Ã , Ã¯Ã®Ã¤Ã¯Ã¨Ã¸Ã¨Ã±Ã¼ Ã­ Ã±Ã®Ã®Ã¡Ã¹Ã¥Ã±Ã²Ã¢Ã®! (peer_id: {$peer_id})"); 
     echo('ok'); 
     break; 
   default: 
@@ -54,3 +54,4 @@ function api($method, $params) {
   } 
   return $response['response']; 
 }
+/?>
